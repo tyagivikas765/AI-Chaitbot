1dc9d71776ab69fdc1712bb1538da895c05f3e1a
tyagivikas765 <tyagivikas765@gmail.com>
2024-03-22 13:02:00 +0530

    added structure for chaitbot

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from './registration'
 import Login from './login';
 import Home from './home';
+import Chatbot from './Chatbot';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Home />} />
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Home />} />
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <div>
+        <h1>AI Chatbot</h1>
+        <Chatbot />
+      </div>
     </div>
   )
 }
